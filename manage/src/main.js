ea632941b0f9c391832b724b6a39aae2f090deed
Qin <mm739023340@qq.com>
2019-12-16 15:18:05 +0800

    完成复杂表格的编写

--- manage/src/main.js
+++ manage/src/main.js
@@ -5,11 +5,14 @@ import store from "./store";
 import './style/index.scss'
 import './plugins/element'
 
-import animated from ''
+import animated from 'animate.css'
+import 'assets/iconfont/iconfont.css'
 /*  引入滑块*/
 import SlideVerify from 'vue-monoplasty-slide-verify'
 
 Vue.use(SlideVerify)
+
+Vue.use(animated)
 
 Vue.config.productionTip = false
 
