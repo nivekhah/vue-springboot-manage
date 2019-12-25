89f2d8520b19e65b163b60c2973ce90b660602b4
huyutai <1146528714@qq.com>
2019-12-25 22:19:12 +0800

    初步完成对接

--- manage/src/api/dashboard.js
+++ manage/src/api/dashboard.js
@@ -1,21 +1,21 @@
 import $axios from './index'
 
-export function getCardData() {
-    const url = '/getCardsData'
-    return $axios.get(url)
-}
-
-export function getLineData() {
-    const url = '/getLineData'
-    return $axios.get(url)
-}
-
-export function getTableData() {
-    const url = '/getTableList'
-    return $axios.get(url)
-}
-export function getBarData() {
-    const url = '/getBarData'
-    return $axios.get(url)
-}
+// export function getCardData() {
+//     const url = '/getCardsData'
+//     return $axios.get(url)
+// }
+//
+// export function getLineData() {
+//     const url = '/getLineData'
+//     return $axios.get(url)
+// }
+//
+// export function getTableData() {
+//     const url = '/getTableList'
+//     return $axios.get(url)
+// }
+// export function getBarData() {
+//     const url = '/getBarData'
+//     return $axios.get(url)
+// }
 
--- manage/src/api/table.js
+++ manage/src/api/table.js
@@ -1,33 +1,33 @@
 import $axios from './index'
 
-export function getPageTab1(params) {
-    const url='/getPageData1'
-    return $axios.get(url,params)
-}
-export function getPageTab2(params) {
-    const url='/getPageData2'
-    return $axios.get(url,params)
-}
+// export function getPageTab1(params) {
+//     const url='/getPageData1'
+//     return $axios.get(url,params)
+// }
+// export function getPageTab2(params) {
+//     const url='/getPageData2'
+//     return $axios.get(url,params)
+// }
 
 /*   图表定义接口封装  */
 export function getChartDefiniation(params) {
-    const url='/chartDefinition'
+    const url='/chartDefinition/getTable'
     return $axios.get(url,params)
 }
 
 /*  数据字典接口封装 */
 export function getDataDictionary(params) {
-    const url='/dataDictionary'
+    const url='/datadictionary/getTable'
     return $axios.get(url,params)
 }
 
 /* 数据源接口封装 */
 export function getDataSouce(params) {
-    const url = '/datasouce'
+    const url = '/datasouce/getTable'
     return $axios.get(url,params)
 }
 /* 查询定义接口封装 */
 export function getQueryDefiniation(params) {
-    const url = '/queryDefination'
+    const url = '/QueryDefination/getTable'
     return $axios.get(url,params)
 }
